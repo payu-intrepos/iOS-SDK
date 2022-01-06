431f9569ece4568aa6bd30c73d61ad851f55d826
Shubham Garg <shubham.garg@payu.in>
2022-01-06 19:27:06 +0530

    Support Swift Package Manager with dependency

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PayUIndia-PG-SDK",
-            targets: ["PayUIndia-PG-SDK"]),
+            targets: ["PayUIndia-PG-SDKTarget"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,6 +18,18 @@ let package = Package(
         .package(name: "PayUIndia-NetworkReachability", url: "https://github.com/payu-intrepos/PayUNetworkReachability-iOS", from: "2.0.0")
     ],
     targets: [
+        
+        .target(
+            name: "PayUIndia-PG-SDKTarget",
+            dependencies: [
+                .target(name: "PayUIndia-PG-SDK", condition: .when(platforms: [.iOS])),
+                .product(name: "PayUIndia-CrashReporter", package: "PayUIndia-CrashReporter"),
+                .product(name: "PayUIndia-PayUParams", package: "PayUIndia-PayUParams"),
+                .product(name: "PayUIndia-NetworkReachability", package: "PayUIndia-NetworkReachability")
+            ],
+            path: "PayUIndia-PG-SDKWrapper"
+        ),
+        
             .binaryTarget(name: "PayUIndia-PG-SDK", path: "./PayUBizCoreKit.xcframework")
-        ]
+    ]
 )
